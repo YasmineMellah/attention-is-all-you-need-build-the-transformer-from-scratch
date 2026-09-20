d462703ab2caf73e4727645b74c8e72f856b754c
YasmineMellah <57048633+YasmineMellah@users.noreply.github.com>
2026-09-20 22:35:02 +0100

    Step 5: pad_id_sequence

--- model.py
+++ model.py
@@ -41,8 +41,13 @@ def decode_ids_to_tokens(ids, id_to_token):
     tokens = [id_to_token[id] for id in ids]
     return tokens
 
-# Step 5 - pad_id_sequence (not yet solved)
-# TODO: implement
+# Step 5 - pad_id_sequence
+def pad_id_sequence(ids, max_len, pad_id):
+    len_ids = len(ids)
+    if len_ids < max_len:
+        len_diff = max_len - len_ids
+        return ids + [pad_id] * len_diff
+    return ids[:max_len]
 
 # Step 6 - stack_padded_sequences_to_batch (not yet solved)
 # TODO: implement
