3ef038d6d74ec9817c80f0000f1db2a916243905
YasmineMellah <57048633+YasmineMellah@users.noreply.github.com>
2026-09-21 12:51:29 +0100

    Step 8: compute_positional_div_term

--- model.py
+++ model.py
@@ -65,8 +65,17 @@ def scale_embeddings_by_sqrt_d_model(embeddings, d_model):
     """Scale a token embedding tensor by sqrt(d_model)."""
     return embeddings * math.sqrt(d_model)
 
-# Step 8 - compute_positional_div_term (not yet solved)
-# TODO: implement
+# Step 8 - compute_positional_div_term
+import torch
+
+def compute_positional_div_term(d_model):
+    # TODO: return a 1D FloatTensor of length d_model // 2 holding the sinusoidal frequency divisors    
+    len_out = d_model // 2
+    res = []
+    for i in range(len_out):
+        res_i = 10000**((-2 * i)/d_model)
+        res.append(res_i)
+    return torch.tensor(res)
 
 # Step 9 - build_position_index_column (not yet solved)
 # TODO: implement
