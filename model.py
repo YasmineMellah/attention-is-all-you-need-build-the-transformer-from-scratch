8410eccecb0377da5aedf80c94d5c8b350e6bb5f
YasmineMellah <57048633+YasmineMellah@users.noreply.github.com>
2026-09-23 18:26:59 +0100

    Step 36: normalize_and_scale_with_gamma_beta

--- model.py
+++ model.py
@@ -301,8 +301,13 @@ def compute_layer_norm_mean_and_variance(x):
     var = torch.var(x, dim=-1, keepdim=True, correction=0)
     return (mean, var)
 
-# Step 36 - normalize_and_scale_with_gamma_beta (not yet solved)
-# TODO: implement
+# Step 36 - normalize_and_scale_with_gamma_beta
+import torch
+
+def normalize_and_scale_with_gamma_beta(x, gamma, beta, eps=1e-5):
+    mean, var = compute_layer_norm_mean_and_variance(x)
+    standarized_x = (x - mean) / torch.sqrt(var + eps) 
+    return gamma * standarized_x + beta
 
 # Step 37 - apply_residual_add_and_norm (not yet solved)
 # TODO: implement
